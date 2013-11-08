e0f7135c626c49da6d6b13e240048ce4e706fbb7
Jan Erik Hanssen <jhanssen@gmail.com>
2013-11-08 00:17:40 -0800

    fix

--- src/IndexerJob.cpp
+++ src/IndexerJob.cpp
@@ -15,7 +15,7 @@ IndexerJob::IndexerJob(IndexType t, const Path &p, const Source &s, const String
 }
 
 IndexerJob::IndexerJob()
-    : state(Pending), port(0), type(Invalid), process(0), started(0), complete(false)
+    : state(Pending), port(0), type(Invalid), process(0), id(0), started(0), complete(false)
 {
 }
 
