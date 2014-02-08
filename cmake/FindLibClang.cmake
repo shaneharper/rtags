67210e5b2503361ce41c853bd3a714a1143bef80
Shane Harper <shane@shaneharper.net>
2014-02-08 23:03:52 +1100

    .
    Include directory was specified twice.

--- cmake/FindLibClang.cmake
+++ cmake/FindLibClang.cmake
@@ -12,7 +12,6 @@ find_path(CLANG_INCLUDE clang-c/Index.h
           /opt/local/libexec/llvm-3.3/include
           /opt/local/libexec/llvm-3.2/include
           /opt/local/libexec/llvm-3.1/include
-          /usr/lib/llvm-3.4/include
           NO_DEFAULT_PATH)
 if (NOT EXISTS ${CLANG_INCLUDE})
   find_path(CLANG_INCLUDE clang-c/Index.h)
